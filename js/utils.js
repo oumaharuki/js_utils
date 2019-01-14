3860ae33b9410283c96e70d4b5b613f515219f76
oumaharuki <1781808576@qq.com>
2019-01-14 10:17:13 +0800

    增加css方法

---
+++ js/utils.js
@@ -0,0 +1,38 @@
+var utils=(function () {
+    //get ele css
+    var getCss=function (ele, attr) {
+        var val = null;
+        val='getComputedStyle' in window?window.getComputedStyle(ele)[attr]:ele.currentStyle[attr];
+        var reg = /^-?\d+(\.\d+)?(px|pt|em|rem)?$/i;
+        reg.test(val) ? val = parseFloat(val) : null;
+        return val;
+    }
+    //set ele css
+    var setCss = function (ele, attr, value) {
+        !isNaN(value) && !/^(zIndex|opacity)$/i.test(attr) ? value += 'px' : null;
+        ele['style'][attr] = value;
+    };
+    //set ele group css
+    var setGroupCss = function (ele, options) {
+        for (var key in options) {
+            if (options.hasOwnProperty(key)) {
+                setCss(ele, key, options[key]);
+            }
+        }
+    };
+    //set or get ele css
+    var css = function () {
+        var len = arguments.length,
+            fn = getCss;
+        var arg=[].slice.call(arguments);
+        len >= 3 ? fn = setCss : null;
+        len === 2 && (typeof arg[1] === 'object') ? fn = setGroupCss : null;
+        return fn.apply(null,arg);
+    };
+    return {
+        getCss:getCss,
+        setCss:setCss,
+        setGroupCss:setGroupCss,
+        css:css
+    }
+})();
